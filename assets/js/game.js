26101bcb72378dc6c4e730163f8b42e12567b2fd
GLuna1982 <gluna0705@gmail.com>
2022-05-21 18:46:06 -0400

    change input options for shop to numeric values

--- assets/js/game.js
+++ assets/js/game.js
@@ -160,24 +160,21 @@ var endGame = function() {
 var shop = function() {
     // ask player what they'd like to do
     var shopOptionPrompt = window.prompt (
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter: 1 to REFILL, 2 to UPGRADE, or 3 to LEAVE."
     );
+
+    shopOptionPrompt = parseInt(shopOptionPrompt);
 
     //use switch to carry out action
     switch (shopOptionPrompt) {
-        case "REFILL": //new case, since there's no break it will fall through the next case
-        case "refill":
+        case 1: //new case, since there's no break it will fall through the next case
             playerInfo.refillHealth();            
             break;
-        case "UPGRADE": //new case, since there's no break it will fall through the next case
-        case "upgrade":
+        case 2: //new case, since there's no break it will fall through the next case
             playerInfo.upgradeAttack();            
             break;
-        case "LEAVE": //new case, since there's no break it will fall through the next case
-        case "leave":
+        case 3: //new case, since there's no break it will fall through the next case
             window.alert("Leaving store.");
-
-            //do nothing, so function will end
             break;
         default:
             window.alert("You did not pick a valid option. Try again.");
